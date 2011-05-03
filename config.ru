38b52a474e4b02d6914272dc4194245493aab39a
Jake Ross <thumbsupguy@frontier.com>
2011-05-02 18:45:07 -0700

    deleted unused files

--- config.ru
+++
@@ -1,5 +0,0 @@
-require 'sinatra'
-
-require File.join(File.dirname(__FILE__), 'game')
-
-run Sinatra::Application
--- public/game.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    font_size = 16;
-    $('#bigger').click(function (){
-        font_size += 4;
-        $('p').css('font-size', font_size + "px");
-    });
-})
